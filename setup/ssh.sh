e507d74daa4147ffc81379edff810dcc4f9afbf0
Paul Shryock <pshryock@nba.com>
2020-08-24 08:14:21 -0400

    Update SSH

--- setup/ssh.sh
+++ setup/ssh.sh
@@ -2,3 +2,11 @@
 
 # Add SSH keys
 ssh-keygen
+
+# Copies the contents of the id_rsa.pub file to your clipboard
+pbcopy < ~/.ssh/id_rsa.pub
+
+open https://github.com
+# In the upper-right corner of any page, click your profile photo, then click Settings.
+# In the user settings sidebar, click SSH and GPG keys.
+# Click New SSH key or Add SSH key.
